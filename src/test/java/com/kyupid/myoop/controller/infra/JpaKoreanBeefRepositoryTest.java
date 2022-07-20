1020ef722d4384dcbc6886c7eb4b5cb2f410f9ae
kyupid <iamscvv@gmail.com>
2022-07-20 18:08:09 +0900

    test: JpaKoreanBeefRepository::findById에 대한 단위 테스트 추가

--- src/test/java/com/kyupid/myoop/controller/infra/JpaKoreanBeefRepositoryTest.java
+++ src/test/java/com/kyupid/myoop/controller/infra/JpaKoreanBeefRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,6 +24,8 @@ class JpaKoreanBeefRepositoryTest {
     private final String PRODUCT_NAME = "세상에서 제일 맛있는 한우";
     private final BigDecimal PRICE = new BigDecimal(1000);
     private final String MEAT_GRADE = "1+";
+    private final Long PRODUCT_ID = 1L;
+    private final Long PRODUCT_ID_NOT_EXISTING = 100L;
 
     private KoreanBeef product;
 
@@ -41,7 +44,7 @@ class JpaKoreanBeefRepositoryTest {
     @DisplayName("findAll 메소드는")
     class Describe_findAll {
         private List<KoreanBeef> subject() {
-            return repositoryt.findAll();
+            return repository.findAll();
         }
 
         @Nested
@@ -66,6 +69,43 @@ class JpaKoreanBeefRepositoryTest {
             @DisplayName("비어 있지 않은 List를 반환한다")
             void it_returns_not_empty_list() {
                 assertThat(subject()).isNotEmpty();
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("findById 메소드는")
+    class Describe_findById {
+        private Optional<KoreanBeef> subjectWithExistingId() {
+            return repository.findById(PRODUCT_ID);
+        }
+
+        @Nested
+        @DisplayName("존재하는 상품 아이디를 전달 받는다면")
+        class Context_with_existing_product {
+            @BeforeEach
+            void setUp() {
+                repository.save(product);
+            }
+
+            @Test
+            @DisplayName("비어있지 않은 값을 반환한다")
+            void it_returns_not_empty() {
+                assertThat(subjectWithExistingId()).isPresent();
+            }
+        }
+
+        @Nested
+        @DisplayName("존재하지않는 상품아이디를 받는다면")
+        class Context_without_existing_product {
+            private Optional<KoreanBeef> subjectWithNotExistingId() {
+                return repository.findById(PRODUCT_ID_NOT_EXISTING);
+            }
+
+            @Test
+            @DisplayName("빈 값을 반환한다")
+            void it_returns_empty() {
+                assertThat(subjectWithNotExistingId()).isNotPresent();
             }
         }
     }
