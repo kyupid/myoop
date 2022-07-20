c38eb5bf60aa5f507de7a45dbd0f43bdd4258e72
kyupid <iamscvv@gmail.com>
2022-07-20 17:09:43 +0900

    init

---
+++ src/main/java/com/kyupid/myoop/MyoopApplication.java
@@ -0,0 +1,13 @@
+package com.kyupid.myoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyoopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyupid/myoop/MyoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyupid.myoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
